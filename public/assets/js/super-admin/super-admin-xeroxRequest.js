d94f7204b16cd0d4099478262772557c929daeec
Vinayak KS <vinayachar007@gmail.com>
2018-07-17 17:12:41 +0530

    added super admin feature

---
+++ public/assets/js/super-admin/super-admin-xeroxRequest.js
@@ -0,0 +1,64 @@
+
+// firebase =  require("firebase") ; 
+// Initialize Firebase
+  var config = {
+		apiKey: "AIzaSyC5RdTPHSkQh3JeIqkYjXGQWsfBEFSGcVg",
+		authDomain: "xeroxapp-a0093.firebaseapp.com",
+		databaseURL: "https://xeroxapp-a0093.firebaseio.com",
+		storageBucket: "gs://xeroxapp-a0093.appspot.com"
+	  };
+	  firebase.initializeApp(config);
+
+
+
+ref = firebase.database().ref('/xerox') ;
+function createUserFromRow(xyz){
+
+  alert(xyz);
+}
+console.log("This is doing ! ") ;
+ref.once('value', snap=>{
+  obj = snap.val() ;
+  
+  Object.keys(obj).map((sample_no , index) =>{
+    data = obj[sample_no] ;
+
+ 
+    
+    $('#xeroxRequestEntries').append(`
+    <tr>
+    <td class="text-center">${sample_no}</td>
+    <td><span class="font-medium">${data.name}</span>
+       
+    <td>Visual Designer
+        <br/><span class="text-muted">Past : teacher</span></td>
+    <td>${data.xemail}
+        <br/><span class="text-muted">${data.pnumber}</span></td>
+    <td>15 Mar 1988
+        <br/><span class="text-muted">10: 55 AM</span></td>
+    <td>
+        <select class="form-control">
+            <option>Modulator</option>
+            <option>Admin</option>
+            <option>User</option>
+            <option>Subscriber</option>
+        </select>
+    </td>
+    <td>
+        <button type="button" class="btn btn-info btn-outline btn-circle btn-lg m-r-5"><i class="ti-key" onCLick="createUserFromRow('${data.xemail}');"></i></button>
+        <button type="button" class="btn btn-info btn-outline btn-circle btn-lg m-r-5"><i class="ti-trash"></i></button>
+        <button type="button" class="btn btn-info btn-outline btn-circle btn-lg m-r-5"><i class="ti-pencil-alt"></i></button>
+        <button type="button" class="btn btn-info btn-outline btn-circle btn-lg m-r-20"><i class="ti-upload"></i></button>
+    </td>
+</tr>
+    `) ; 
+  })
+
+  
+  
+})
+
+
+
+
+
